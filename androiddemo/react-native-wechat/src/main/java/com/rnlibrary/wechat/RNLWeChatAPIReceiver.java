396655c1cc3d0906586a56653eb1c253a90a7254
pencilCool <yhtangcoder@gmail.com>
2023-05-17 09:12:56 +0800

    change import src to demo android app

--- androiddemo/react-native-wechat/src/main/java/com/rnlibrary/wechat/RNLWeChatAPIReceiver.java
+++
@@ -1,9 +0,0 @@
-package com.rnlibrary.wechat;
-
-import com.facebook.react.bridge.ReadableMap;
-
-public interface RNLWeChatAPIReceiver {
-    void onReqFromWeChat(ReadableMap payload);
-
-    void onRespFromWeChat(String name, boolean isSuccess, ReadableMap payload);
-}
